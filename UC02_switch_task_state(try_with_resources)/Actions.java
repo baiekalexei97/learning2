a8aa25bd320184e225929d9110859b2019b0bc72
baiekalexei97 <baiekalexei97@yandex.ru>
2019-12-05 15:49:21 +0300

    java

--- UC02_switch_task_state(try_with_resources)/Actions.java
+++ UC02_switch_task_state(try_with_resources)/Actions.java
@@ -7,116 +7,144 @@ import java.util.ArrayList;
 
 public class Actions
 {
-    public int init() throws Throwable {
+    public int init(){
     	
     	return 0;
     }
     
-    public int action() throws ClassNotFoundException, SQLException {
+    public int action(){
+		
     	ArrayList<String> id = new ArrayList<String>();
-    	
-    	try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-        } catch (ClassNotFoundException ex) {
-	    	lr.log_message("Database Driver not found");
-	    	lr.abort();
-    	}
     	
     	lr.save_string(lr.eval_string("jdbc:oracle:thin:@" +
     		                              "{host}:{port}:{sid}"), "url");
     	
     	try (Connection connection = DriverManager.getConnection(lr.eval_string("{url}"),
     		                                         lr.eval_string("{login}"),
-    		                                         lr.eval_string("{password}"))) {
+    		                                         lr.eval_string("{password}"));
+    		                                         Statement stmt = connection.createStatement()) {
+    		
     		lr.log_message("JDBC Connection Successful");
+    		
     		connection.setAutoCommit(false);
-    		try(Statement stmt = connection.createStatement()) {
-			   
-    			try(ResultSet rset = stmt.executeQuery("select id from ticket " +
+    		
+    		try(ResultSet rset = stmt.executeQuery("select id from ticket " +
     			                                       "where state_id = -1" +
     			                                       "and text like '%(alex)'")){
-    				lr.log_message("Selecting Ticket");
-    				
-    				while (rset.next()){
-    					id.add(rset.getString("id"));
-    				}
-    				
-    			}catch (SQLException e_rset) {
-    				e_rset.printStackTrace();
-    				lr.log_message("Couldn't select ticket, " +
-    				               "Exception - " + e_rset.getMessage());
-    				return 1;
-    			}
     			
-	    		lr.log_message("Updating Ticket Status");
-	    		System.out.println(id.get(0));
-	    		
-	    		stmt.executeQuery("update ticket set state_id = 1, external_system = 'ASKO' " +
-	    		                  "where id="+id.get(0));
-	    		
-			  
-	    		lr.log_message("Adding Ticket to Tasks");		   
-	    		stmt.executeQuery("insert into task(id," +
-	    		                  "change_id," +
-	    		                  "ticket_id," +
-	    		                  "guid," +
-	    		                  "header," +
-	    		                  "text," +
-	    		                  "priority_id," +
-	    		                  "state_id," +
-	    		                  "client_id," +
-	    		                  "solution_group_id," +
-	    		                  "create_date," +
-	    		                  "sync_mask," +
-	    		                  "last_edit_date," +
-	    		                  "last_edit_user_login," +
-	    		                  "engineer_id," +
-	    		                  "contractor_id," +
-	    		                  "external_id," +
-	    		                  "external_system) " +
-	    		                  "select id, " +
-	    		                  "'IDC2D620524153zdzPWAoX9OFgW4UB', " +
-	    		                  "id, " +
-	    		                  "'d830c5ee-9b77-4bd1-879a-0c4d2c282a67', " +
-	    		                  "header, " +
-	    		                  "text, " +
-	    		                  "'3', " +
-	    		                  "'1', " +
-	    		                  "applicant_id, " +
-	    		                  "'9', " +
-	    		                  "create_date," +
-	    		                  "'0', " +
-	    		                  "last_edit_date, " +
-	    		                  "last_edit_user_login," +
-	    		                  "'103'," +
-	    		                  "'102'," +
-	    		                  "'TSK_1800000', " +
-	    		                  "external_system" +
-	    		                  " from ticket where id = "+id.get(0));
-			 	
-	    		lr.log_message("Commit");
-	    		connection.commit();
-    		} catch (SQLException e1) {
-	    		e1.printStackTrace();
-	    		lr.log_message("Rolling back, " +
-	    		               "Exception - " + e1.getMessage());
-	    		try{
-	    			connection.rollback();
-	    		}catch(SQLException e2){
-	    			e2.printStackTrace();
-	    		}
-		    	return 1;
+    			lr.log_message("Selecting Ticket");
+    			
+    			while (rset.next()) id.add(rset.getString("id"));
+    			
+    			lr.log_message("Selected Ticket #" + id.get(0));
+    		
+    		}catch (SQLException e_rset1) {
+    			
+    			e_rset1.printStackTrace();
+    			
+    			lr.log_message("Could Not Select Ticket, " +
+    			               "Exception: " + e_rset1.getMessage());
+    			
+    			return 1;
     		}
-    		    		
+    		
+    		lr.log_message("Updating Ticket Status");
+    		
+    		try(ResultSet rset = stmt.executeQuery("update ticket " +
+    		                                       "set state_id = 1, " +
+    		                                       "external_system = 'ASKO' " +
+    		                                       "where id = "+id.get(0))){
+    		lr.log_message("Status Updated");
+    		
+    		}catch (SQLException e_rset2) {
+    			
+    			e_rset2.printStackTrace();
+    			
+    			lr.log_message("Status Update Failed, " +
+    			               "Exception: " + e_rset2.getMessage());
+    			
+    			lr.log_message("Rollback");
+    			
+    			connection.rollback();
+    			
+    			return 1;
+    		}
+    		
+    		lr.log_message("Inserting New Task");
+    		
+    		try(ResultSet rset = stmt.executeQuery("insert into task(id," +
+    		                                       "change_id," +
+    		                                       "ticket_id," +
+    		                                       "guid," +
+					    		                   "header," +
+					    		                   "text," +
+					    		                   "priority_id," +
+					    		                   "state_id," +
+					    		                   "client_id," +
+					    		                   "solution_group_id," +
+					    		                   "create_date," +
+					    		                   "sync_mask," +
+					    		                   "last_edit_date," +
+					    		                   "last_edit_user_login," +
+					    		                   "engineer_id," +
+					    		                   "contractor_id," +
+					    		                   "external_id," +
+					    		                   "external_system) " +
+					    		                   "select id, " +
+					    		                   "'IDC2D620524153zdzPWAoX9OFgW4UB', " +
+					    		                   "id, " +
+					    		                   "'d830c5ee-9b77-4bd1-879a-0c4d2c282a67', " +
+					    		                   "header, " +
+					    		                   "text, " +
+					    		                   "'3', " +
+					    		                   "'1', " +
+					    		                   "applicant_id, " +
+					    		                   "'9', " +
+					    		                   "create_date," +
+					    		                   "'0', " +
+					    		                   "last_edit_date, " +
+					    		                   "last_edit_user_login," +
+					    		                   "'103'," +
+					    		                   "'102'," +
+					    		                   "'TSK_1800000', " +
+					    		                   "external_system " +
+					    		                   "from ticket " +
+					    		                   "where id = "+id.get(0))){
+    		    	
+    		    	lr.log_message("New Task Inserted");
+    		
+    		}catch (SQLException e_rset3) {
+    			
+    			e_rset3.printStackTrace();
+    			
+    			lr.log_message("Insert Operation Failed, " +
+    			               "Exception: " + e_rset3.getMessage());
+    			
+    			lr.log_message("Rollback");
+    			
+    			connection.rollback();
+    			
+    			return 1;
+    		}
+    		
+    		lr.log_message("Commit");
+    		
+    		connection.commit();
+    		    		    		
     	} catch (SQLException e) {
+    		
     		e.printStackTrace();
-    		lr.log_message("Database Connection Failed, " +
-    		               "Please check your connection string");
+    		
+    		lr.log_message("Could Not Connect, " +
+    		               "Exception: " + e.getMessage());
+    		
     		lr.abort();
     	}
-    	return 0;
+    	
+    	return 0;	
     }
-    public int end() throws Throwable {
+	
+    public int end(){
     	
     	return 0;
     }
